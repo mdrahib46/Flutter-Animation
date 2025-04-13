3d06797e1ac1555d3520d36ad818557657106194
mdrahib46 <nahid179846@bsdi-bd.org>
2025-04-13 14:29:09 +0600

    Project Initiation for Flutter Animation

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/presentation/home_page.dart';
+
+
+class FlutterAnimation extends StatelessWidget {
+  const FlutterAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/app.dart';
+
+void main() {
+  runApp(const FlutterAnimation());
+}
+
